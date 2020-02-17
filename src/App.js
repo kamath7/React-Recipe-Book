55bbcd6fbdeb75ddf804b672388a5ac2a9ebe25f
Adithya Kamath <adithyakamath@Adithyas-MacBook-Air.local>
2020-02-17 11:39:13 +0530

    Changes to API

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = ()=>{
   },[query]);
 
   const getRecipes = async()=>{
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3`);
     const data = await response.json();
     setRecipes(data.hits);
   };
